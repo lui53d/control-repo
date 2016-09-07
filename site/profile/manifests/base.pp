3d5c2a286b474f8a05c84257034af42b08abb018
Luis Eduardo Ibáñez Pérez <luised@localhost.localdomain>
2016-09-07 12:40:49 +0200

    Add profile config again

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -2,9 +2,9 @@ class profile::base {
 
   #the base profile should include component modules that will be on all nodes
 
-  class { '::ntp':
-    servers => [ 'ntp1.corp.com', 'ntp2.corp.com' ],
-  }
+  #class { '::ntp':
+  #  servers => [ 'ntp1.corp.com', 'ntp2.corp.com' ],
+  #}
 
   network::mroute { 'enp0s8':
     routes => {
